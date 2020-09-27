72b20efa48ee566493bd01d83d427550efca81cd
jasongao97 <jasongao97@gmail.com>
2020-09-28 00:56:01 +0800

    working server

--- week3/public/script.js
+++ week3/public/script.js
@@ -1,67 +1,190 @@
+// script.js
+
+const socket = io.connect();
+
+// get canvas and set width and height
+const canvas = document.getElementById("gameWindow");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const ctx = canvas.getContext("2d");
+// for better image smooth quality
+ctx.imageSmoothingQuality = "high";
+
+const characters = [
+  "elephant",
+  "giraffe",
+  "hippo",
+  "monkey",
+  "panda",
+  "parrot",
+  "penguin",
+  "pig",
+  "rabbit",
+  "snake",
+];
+
+// const charactersImage = {
+//   "elephant": "https://cdn.glitch.com/c99d9875-2231-49d9-89bb-07e2a3d9845e%2Felephant.png?v=1601141192796",
+//   "giraffe": "https://cdn.glitch.com/c99d9875-2231-49d9-89bb-07e2a3d9845e%2Fgiraffe.png?v=1601141192848",
+//   "hippo": "https://cdn.glitch.com/c99d9875-2231-49d9-89bb-07e2a3d9845e%2Fhippo.png?v=1601141192776",
+//   "monkey": "https://cdn.glitch.com/c99d9875-2231-49d9-89bb-07e2a3d9845e%2Fmonkey.png?v=1601141193282",
+//   "panda": "https://cdn.glitch.com/c99d9875-2231-49d9-89bb-07e2a3d9845e%2Fpanda.png?v=1601141194056",
+//   "parrot": "https://cdn.glitch.com/c99d9875-2231-49d9-89bb-07e2a3d9845e%2Fparrot.png?v=1601141192003",
+//   "penguin": "https://cdn.glitch.com/c99d9875-2231-49d9-89bb-07e2a3d9845e%2Fpenguin.png?v=1601141192024",
+//   "pig": "https://cdn.glitch.com/c99d9875-2231-49d9-89bb-07e2a3d9845e%2Fpig.png?v=1601141194514",
+//   "rabbit": "https://cdn.glitch.com/c99d9875-2231-49d9-89bb-07e2a3d9845e%2Frabbit.png?v=1601141195097",
+//   "snake": "https://cdn.glitch.com/c99d9875-2231-49d9-89bb-07e2a3d9845e%2Fsnake.png?v=1601141195813",
+// }
+
+const keyState = {};
+
 let planetImage;
 let angle = 0;
+let myId;
 
 let me;
 let friends = [];
 
-function preload() {
-  planetImage = loadImage("assets/planet.png");
+const loadImage = (src) => {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => resolve(img);
+    img.onerror = reject;
+  });
+};
 
-  me = new Character("penguin");
-  friends = [
-    new Character("parrot", 20),
-    new Character("giraffe", -30)
-  ]
-}
+const drawImage = (image, x, y, angle, scale) => {
+  const w = scale ? image.width * scale : image.width;
+  const h = scale ? image.height * scale : image.height;
 
-function setup() {
-  // create the canvas to be the full size of the window
-  createCanvas(innerWidth, innerHeight);
+  ctx.save();
+  ctx.translate(x, y);
+  ctx.rotate((Math.PI / 180) * angle);
+  ctx.drawImage(image, -w / 2, -h / 2, w, h);
+  ctx.restore();
+};
 
-  background(0);
-}
+/*
+ * move myself - make sure that angle is always in the range of [0, 360]
+ * @param offset
+ */
+const move = (offset) => {
+  angle += offset;
 
-function draw() {
-  background(0);
-  translate(width / 2, height / 2);
-
-  drawPlanet();
-  friends.map(e => e.draw())
-  me.draw();
-
-  // Arrow Keys or WASD to move
-  if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) {
-    angle += .5;
-  }
-  if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) {
-    angle -= .5;
-  }
-
-  // always sync with global angle
+  if (angle > 360) angle -= 360;
+  if (angle < 0) angle += 360;
   me.angle = angle;
-}
 
-function drawPlanet() {
-  translate(0, 660);
-  rotate((PI / 180) * angle);
-  imageMode(CENTER);
-  image(planetImage, 0, 0, 1200, 1200);
-  rotate(-(PI / 180) * angle);
-  translate(0, -660);
-}
+  socket.emit("move", angle);
+};
 
 class Character {
-  constructor(avatar, angle) {
-    this.image = loadImage(`assets/characters/${avatar}.png`);
-    this.angle = angle || 0;
+  constructor(avatar, angle, id) {
+    return (async () => {
+      this.id = id;
+      this.image = await loadImage(`assets/characters/${avatar}.png`);
+      this.angle = angle || 0;
+      return this;
+    })();
   }
 
-  // draw character @ different angle
   draw() {
-    translate(0, 660);
-    rotate((PI / 180) * (angle - this.angle));
-    image(this.image, 0, -610, 60, 60);
-    rotate(-(PI / 180) * (angle - this.angle));
-    translate(0, -660);
+    // @different angle
+    const angleOffset = angle - this.angle;
+    const r = planetImage.width / 2;
+
+    // calculate characters position and draw
+    const x = r * Math.sin((angleOffset * Math.PI) / 180);
+    const y = r * (1 - Math.cos((angleOffset * Math.PI) / 180));
+    drawImage(this.image, x, y, angleOffset, 0.2);
   }
 }
+
+const setup = async () => {
+  planetImage = await loadImage("assets/planet.png");
+
+  const character = characters[Math.floor(Math.random() * characters.length)];
+  angle = Math.floor(Math.random() * 360);
+  me = await new Character(character, angle);
+
+  socket.emit("join", { character, angle });
+};
+
+const draw = () => {
+  ctx.save();
+  ctx.fillStyle = "#000";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.translate(canvas.width / 2, canvas.height / 2);
+
+  drawImage(planetImage, 0, 660, angle);
+
+  friends.map((e) => e.draw());
+  me.draw();
+
+  if (keyState["ArrowLeft"] || keyState["a"]) {
+    move(0.5);
+  }
+  if (keyState["ArrowRight"] || keyState["d"]) {
+    move(-0.5);
+  }
+
+  ctx.font = "30px Arial";
+  ctx.fillStyle = "#fff";
+  ctx.fillText(`Current angle: ${angle}`, 0, -200);
+
+  ctx.restore();
+  window.requestAnimationFrame(draw);
+};
+
+window.addEventListener("keydown", (event) => {
+  keyState[event.key] = true;
+});
+
+window.addEventListener("keyup", (event) => {
+  keyState[event.key] = false;
+});
+
+/*
+ * main
+ * setup and start drawing
+ */
+setup().then(draw);
+
+const initFriends = async (people) => {
+  friends = [];
+
+  people
+    .filter((e) => e.id !== myId)
+    .forEach(async (person) => {
+      friends.push(
+        await new Character(person.character, person.angle, person.id)
+      );
+    });
+};
+
+socket.on("login", (data) => {
+  myId = data.myId;
+  initFriends(data.people);
+});
+
+socket.on("join", async (person) => {
+  friends.push(
+    await new Character(person.character, person.angle, person.id)
+  )
+});
+
+socket.on("move", (data) => {
+  const index = friends.findIndex((e) => e.id === data.id);
+  if (index > -1) {
+    friends[index].angle = data.angle;
+  }
+});
+
+socket.on("quit", (id) => {
+  const index = friends.findIndex((e) => e.id === id);
+  if (index > -1) {
+    friends.splice(index, 1);
+  }
+})
--- week3/server.js
+++ week3/server.js
@@ -1,17 +1,17 @@
 // Express is a node module for building HTTP servers
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // Tell Express to look in the "public" folder for any files first
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // If the user just goes to the "route" / then run this function
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get("/", function (req, res) {
+  res.send("Hello World!");
 });
 
-// Here is the actual HTTP server 
-var http = require('http');
+// Here is the actual HTTP server
+var http = require("http");
 // We pass in the Express object
 var httpServer = http.createServer(app);
 // Listen on port 80
@@ -19,27 +19,52 @@ httpServer.listen(8080);
 
 // WebSocket Portion
 // WebSockets work with the HTTP server
-var io = require('socket.io').listen(httpServer);
+var io = require("socket.io").listen(httpServer);
+
+const people = [];
 
 // Register a callback function to run when we have an individual connection
 // This is run for each individual user that connects
-io.sockets.on('connection', 
-	// We are given a websocket object in our function
-	function (socket) {
-	
-		console.log("We have a new client: " + socket.id);
-		
-		// When this user emits, client side: socket.emit('otherevent',some data);
-		socket.on('chatmessage', function(data) {
-			// Data comes in as whatever was sent, including objects
-			console.log("Received: 'chatmessage' " + data);
-			
-			// Send it to all of the clients
-			socket.broadcast.emit('chatmessage', data);
-		});
-		
-		socket.on('disconnect', function() {
-			console.log("Client has disconnected " + socket.id);
-		});
-	}
+io.sockets.on(
+  "connection",
+  // We are given a websocket object in our function
+  function (socket) {
+    socket.emit("login", { myId: socket.id, people });
+
+    // client join
+    socket.on("join", function (data) {
+      const { angle, character } = data;
+
+      const person = {
+        id: socket.id,
+        angle,
+        character,
+      };
+      people.push(person);
+      console.log(person.character + " joined the chat.");
+
+      socket.broadcast.emit("join", person);
+    });
+
+    // character move
+    socket.on("move", function (angle) {
+      const index = people.findIndex((e) => e.id === socket.id);
+      if (index > -1) {
+        people[index].angle = angle;
+      }
+
+      socket.broadcast.emit("move", { id: socket.id, angle });
+    });
+
+    // client disconnect
+    socket.on("disconnect", function () {
+      const index = people.findIndex((e) => e.id === socket.id);
+      if (index > -1) {
+        console.log(people[index].character + " left.");
+        people.splice(index, 1);
+      }
+
+      socket.broadcast.emit("quit", socket.id);
+    });
+  }
 );
